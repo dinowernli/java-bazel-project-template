c4564054bb706c4319672beacf00628e95dfe5ce
Dino Wernli <dinowernli@gmail.com>
2015-11-22 16:27:00 +0000

    Add a basic unit test.

---
+++ src/test/java/org/example/library/HelloFormatterTest.java
@@ -0,0 +1,21 @@
+package org.example.library;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class HelloFormatterTest {
+  private HelloFormatter formatter;
+
+  @Before
+  public void setUp() {
+    formatter = new HelloFormatter();
+  }
+
+  @Test
+  public void testNoArgument() {
+    assertEquals("Hello, world", formatter.format("world"));
+  }
+}
+
